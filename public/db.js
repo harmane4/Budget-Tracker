be0e60b37ee8a264f04dc90b6980a7b782040207
Elise Harman <elise_harman@hotmail.com>
2021-07-20 12:54:28 +0800

    Added indexedDB

---
+++ public/db.js
@@ -0,0 +1,64 @@
+let db;
+
+//Opens database
+const request = window.indexedDB.open("budget", 1);
+
+// Creates or updates the version of the database
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  const store = db.createObjectStore("BudgetStore", { autoIncrement: true });
+};
+
+// Handles error
+request.onerror = function (event) {
+  console.log("Database Error:" + event.target.errorCode);
+};
+
+// Want a function that checks database if online
+function checkDatabase() {
+  const transaction = db.transaction(["BudgetStore"], "readwrite");
+  const store = transaction.objectStore("BudgetStore");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          const transaction = db.transaction(["BudgetStore"], "readwrite");
+          const store = transaction.objectStore("BudgetStore");
+          store.clear();
+        });
+    }
+  };
+}
+// Handles success
+request.onsuccess = function (event) {
+  db = event.target.result;
+  // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/onLine
+  if (navigator.onLine) {
+    checkDatabase();
+  } else {
+    console.log("offline");
+  }
+};
+
+// Want a function that saves records
+function saveRecord(record) {
+  //Adding data to database
+  const transaction = db.transaction(["BudgetStore"], "readwrite");
+  const store = transaction.objectStore("BudgetStore");
+  // Put this updated object back into the database.
+  // https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore/put
+  store.add(record);
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
